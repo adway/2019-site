ec4e8e0a86cc593a3cfefeaebf8cd044628e3d2e
Adway Wadekar <adway@adway.io>
2019-04-20 14:53:24 -0400

    update site

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,26 +28,27 @@ export default () => (
       <Link href="https://stopioid.org" target="_blank">
         Stopioid
       </Link>
-      , which is an app that utilizes a decision tree alogrithm to predict and
+      , which is an app that utilizes a random forest alogrithm to predict and
       prevent opioid use disorder. I am an intern at the{' '}
       <Link href="http://edc.town.westborough.ma.us" target="_blank">
         Town of Westborough’s Economic Development Committee
       </Link>
-      . Currently, I’m working on populating a database with all of the
-      businesses in Westborough.
+      . I've just finished populating a database with all of the businesses in
+      Westborough. The next step is to make the database to usable in managing
+      relations with businesses in the Town of Westborough.
     </P>
     <P>
       I am also a contributing writer and photographer for the{' '}
       <Link href="https://communityadvocate.com" target="_blank">
         Community Advocate Newspaper
       </Link>
-      . I conduct projects independently and have presented a full paper and a
-      poster at the{' '}
+      . I conduct research projects independently and have presented a full
+      paper and a poster at the{' '}
       <Link href="https://ewh.ieee.org/conf/stem/" target="_blank">
         Integrated STEM Education Education Conference{' '}
       </Link>
       . I have also won regional, state, and national awards at multiple science
-      fairs. This year, I will present my work at at the{' '}
+      fairs. I am grateful that this year, I will present my work at at both the{' '}
       <Link
         target="_blank"
         href="https://student.societyforscience.org/intel-isef"
