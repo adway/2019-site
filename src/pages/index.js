85df6011a6adad4b7fbdaf4bb6b23c21a2a243fc
Adway Wadekar <adway@adway.io>
2019-11-05 13:20:10 -0500

    Update Resume and Update bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,15 +18,14 @@ const IndexPage = () => (
         Saint John’s High School
       </Link>{' '}
       in Shrewsbury, Massachusetts. I am interested in studying the social
-      sciences using a quantitative approach.
-    </P>
-    <P>
-      I enjoy learning about various topics, reading the news and taking
-      pictures with my Canon Rebel T5. I am an intern at the Town of
-      Westborough’s Economic Development Committee. I've just finished
-      populating a database with all of the businesses in Westborough. The next
-      step is to make the database to usable in managing relations with
-      businesses in the Town of Westborough.
+      sciences using a quantitative approach. Right now, my interests lie in
+      using public domain data to understand mental illnesses and substance
+      abuse. I am also interested in understanding historical texts using a
+      computational approach (a new field!). I conduct research projects
+      independently and have presented a full paper and a poster at the IEEE
+      Integrated STEM Education Education Conference. I've also presented my
+      work at Intel ISEF, the National JSHS and the IEEE International Computer
+      Software and Applications Conference.
     </P>
     <P>
       I am the 2019-2020 Photographer in Residence for the Town of Westborough.
@@ -39,31 +38,25 @@ const IndexPage = () => (
       .
     </P>
     <P>
-      I am also a contributing writer and photographer for the Community
-      Advocate Newspaper. I conduct research projects independently and have
-      presented a full paper and a poster at the IEEE Integrated STEM Education
-      Education Conference. I've also presented my work at Intel ISEF, the
-      National JSHS and the IEEE International Computer Software and
-      Applications Conference.
+      I enjoy learning about various topics, reading the news, and taking
+      pictures with my Canon Rebel T5. I am an intern at the Town of
+      Westborough’s Economic Development Committee. I've just finished
+      populating a database with all of the businesses in Westborough. The next
+      step is to make the database to usable in managing relations with
+      businesses in the Town of Westborough. I am also a contributing writer and
+      photographer for the Community Advocate Newspaper.
     </P>
+    <P></P>
     <P mb={3}>
-      One of my other passions is coding. I've been coding since the 4th grade.
-      I've always been facinated with the web specifically, but I love learning
-      new technologies regardless of their (potential) application. I enjoy
-      running programs to get other students excited about programming and
-      making as well. Currently, I'm organizing{' '}
+      One of my other passions is programming. I've been programming since the
+      4th grade. I've always been facinated with the web specifically, but I
+      love learning new technologies regardless of their (potential)
+      application. I enjoy running programs to get other students excited about
+      programming and making as well. Currently, I'm helping to organize{' '}
       <Link href="https://codeday.org/boston" target="_blank">
         CodeDay Boston
-      </Link>{' '}
-      and I'm working on starting a{' '}
-      <Link href="https://hackclub.com" target="_blank">
-        Hack Club
-      </Link>{' '}
-      at my school.
-    </P>
-    <P>
-      I regularly update and look at my <Link href="/goals">goals</Link> to help
-      me stay on track, stay motivated, work hard, and not procrastinate.
+      </Link>
+      , a program designed to get beginners excited about programming. .
     </P>
   </Layout>
 );
