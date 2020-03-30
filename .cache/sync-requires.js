9498de389487678566df8a2541409a8c2bbe1f9e
Adway Wadekar <adway@adway.io>
2020-03-29 21:26:27 -0400

    let's try this again

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Volumes/Samsung_T5/dev/WebDevelopment/2019-site/.cache/dev-404-page.js"))),
   "component---src-pages-goals-js": hot(preferDefault(require("/Volumes/Samsung_T5/dev/WebDevelopment/2019-site/src/pages/goals.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Volumes/Samsung_T5/dev/WebDevelopment/2019-site/src/pages/index.js"))),
   "component---src-pages-projects-js": hot(preferDefault(require("/Volumes/Samsung_T5/dev/WebDevelopment/2019-site/src/pages/projects.js")))
