a3d7722543c7638b9fcaea285679fa6f650655d3
Adway Wadekar <adway@adway.io>
2020-03-29 20:44:07 -0400

    hello

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,9 +2,9 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Volumes/Samsung_T5/dev/WebDevelopment/2019-site/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-goals-js": () => import("/Volumes/Samsung_T5/dev/WebDevelopment/2019-site/src/pages/goals.js" /* webpackChunkName: "component---src-pages-goals-js" */),
-  "component---src-pages-index-js": () => import("/Volumes/Samsung_T5/dev/WebDevelopment/2019-site/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-projects-js": () => import("/Volumes/Samsung_T5/dev/WebDevelopment/2019-site/src/pages/projects.js" /* webpackChunkName: "component---src-pages-projects-js" */)
+  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-goals-js": () => import("./../src/pages/goals.js" /* webpackChunkName: "component---src-pages-goals-js" */),
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-projects-js": () => import("./../src/pages/projects.js" /* webpackChunkName: "component---src-pages-projects-js" */)
 }
 
